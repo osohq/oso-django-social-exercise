2b86f1789f228bdeb4a37757c7a422fab76c678b
David Hatch <david@osohq.com>
2020-10-21 14:35:35 -0400

    Step 2

--- oso_social/social/views.py
+++ oso_social/social/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseNotAllowed, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
+from django.contrib.auth.models import Group
 
 from .models import Post
 from .forms import PostForm
@@ -12,14 +13,24 @@ from .forms import PostForm
 
 def list_posts(request):
     posts = Post.objects.all().order_by("-created_at")
+    groups = Group.objects.all()
 
     # STEP 1: Add check that user is an admin before they can see a post.
     if not request.user.is_staff:
         posts = []
 
-    return render(request, "social/list.html", {"posts": posts})
+    return render(request, "social/list.html", {"posts": posts, "groups": groups})
 
 # STEP 2: Add group list view.
+def list_group(request, group_id):
+    group = Group.objects.get(id=group_id)
+    posts = Post.objects.filter(group=group_id).order_by("-created_at")
+
+    # Check that user is in the group
+    if not request.user.groups.filter(id=group_id).exists():
+        posts = []
+
+    return render(request, "social/list.html", {"posts": posts, "group": group})
 
 @login_required
 def new_post(request):
